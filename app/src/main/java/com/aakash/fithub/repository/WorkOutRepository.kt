708dcccfebd0b89255a661ba1dd900cc7fa9baaa
Aakash-rai1 <57335739+Aakash-rai1@users.noreply.github.com>
2021-02-28 14:41:36 +0545

    created workout repository

---
+++ app/src/main/java/com/aakash/fithub/repository/WorkOutRepository.kt
@@ -0,0 +1,16 @@
+package com.aakash.fithub.repository
+
+import com.aakash.fithub.api.MyApiRequest
+import com.aakash.fithub.api.ServiceBuilder
+import com.aakash.fithub.api.WorkoutApi
+import com.aakash.fithub.response.WorkOutResponse
+
+class WorkOutRepository : MyApiRequest(){
+    private val WorkoutApi = ServiceBuilder.buildServices(WorkoutApi::class.java)
+
+    suspend fun  getWorkOutApiData(): WorkOutResponse{
+        return apiRequest {
+            WorkoutApi.addWorkout(ServiceBuilder.token!!)
+        }
+    }
+}
