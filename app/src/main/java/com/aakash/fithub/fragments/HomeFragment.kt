540bcb124dce6c4ed7e2eff1473d9290128ac8f1
Aakash-rai1 <57335739+Aakash-rai1@users.noreply.github.com>
2021-02-28 14:35:14 +0545

    slight changes brought in model and home fragments

--- app/src/main/java/com/aakash/fithub/fragments/HomeFragment.kt
+++ app/src/main/java/com/aakash/fithub/fragments/HomeFragment.kt
@@ -5,54 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aakash.fithub.R
+import com.aakash.fithub.adapter.WorkoutHomeAdapter
+import com.aakash.fithub.db.UserDB
+import com.aakash.fithub.entity.Workout
+import com.aakash.fithub.repository.WorkOutRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private lateinit var homeWorkOutrec: RecyclerView
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                ProfileFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+        homeWorkOutrec=view.findViewById(R.id.homeWorkOutrec)
+
+        getData()
+        return view;
+    }
+    fun getData(){
+        try{
+            CoroutineScope(Dispatchers.IO).launch {
+                val WorkOutRepository = WorkOutRepository()
+                val response = WorkOutRepository.getWorkOutApiData()
+                if(response.success==true){
+                    withContext(Main){
+                        println(response)
+                        val workoutitemlist = response.data
+                        UserDB.getInstance(requireContext()).getWorkOutDAO().deleteWorkOutData()
+                        UserDB.getInstance(requireContext()).getWorkOutDAO().insertWorkOutData(response.data)
+                        Toast.makeText(context, "$workoutitemlist", Toast.LENGTH_SHORT).show()
+                        val adapter = WorkoutHomeAdapter(
+                                workoutitemlist as ArrayList<Workout>,
+                                requireContext()
+                        )
+                        homeWorkOutrec.layoutManager = LinearLayoutManager(context)
+                        homeWorkOutrec.adapter = adapter
                     }
                 }
+            }
+        }catch (ex: Exception){
+            Toast.makeText(
+                    context,
+                    "Error : ${ex.toString()}", Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
