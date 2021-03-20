90a23985ae95b1843f6179c961d2a1741d2461ab
Aakash-rai1 <57335739+Aakash-rai1@users.noreply.github.com>
2021-03-20 16:56:40 +0545

    created youtube api in the app

---
+++ app/src/main/java/com/aakash/fithub/YoutubePlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.aakash.fithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YoutubePlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube_player)
+    }
+}
