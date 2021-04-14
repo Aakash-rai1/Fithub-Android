f103474f4485d627729fd9fb80dad4f76ec52a74
Aakash-rai1 <57335739+Aakash-rai1@users.noreply.github.com>
2021-04-14 16:57:22 +0545

    done unittest

--- app/src/test/java/com/aakash/fithub/BMICalculateTest.kt
+++ app/src/test/java/com/aakash/fithub/BMICalculateTest.kt
@@ -1,6 +1,11 @@
 package com.aakash.fithub
 
+import com.aakash.fithub.api.ServiceBuilder
 import com.aakash.fithub.bmi.BMIFormula
+import com.aakash.fithub.entity.User
+import com.aakash.fithub.repository.UserRepository
+import com.aakash.fithub.repository.WorkOutRepository
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 
@@ -8,15 +13,61 @@ import org.junit.Test
 private lateinit var arithmetic: BMIFormula
 
 class BMICalculateTest {
+    private var repository= UserRepository()
     @Test
-    fun testSum(){
-        arithmetic = BMIFormula()
-        arithmetic.height=2F
-        arithmetic.weight= 50F
-
-        val expectedResult =25F
-        val actualResult= arithmetic.bmi()
-        Assert.assertEquals(expectedResult,actualResult)
+    fun checkRegisterUSer() = runBlocking {
+        val user =
+                User(fname = "this is test", email = "thisisemail1mdnsshf212@gmail.com",password = "testing12")
+        repository = UserRepository()
+        val expectedResult = false
+        val response = repository.registerUSer(user)
+        val actualResult = response.success!!
+        Assert.assertEquals(expectedResult, actualResult)
     }
-
+    @Test
+    fun loginUser() = runBlocking {
+        val user = User(email = "aks@gmail.com", password = "12345")
+        repository = UserRepository()
+        val expectedResult = true
+        val response = repository.checkUser(user)
+        val actualResult = response.success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+    @Test
+    fun getme() = runBlocking {
+        val user = User(email = "aks@gmail.com", password = "12345")
+        repository = UserRepository()
+        val expectedResult = true
+        val response = repository.checkUser(user)
+        ServiceBuilder.token = "Bearer " + response.token
+        ServiceBuilder.id = response.id
+        if (response.success == true) {
+            val actualResult = repository.getUser(ServiceBuilder.id!!).success
+            Assert.assertEquals(expectedResult, actualResult)
+        }
+    }
+    @Test
+    fun getAllNotes()= runBlocking {
+        val user = User(email = "aks@gmail.com", password = "12345")
+        repository = UserRepository()
+        val expectedResult = true
+        val response = repository.checkUser(user)
+        ServiceBuilder.token = "Bearer " + response.token
+        ServiceBuilder.id = response.id
+        val repo=WorkOutRepository()
+        val actualResult=repo.getWorkOutApiData().success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+    @Test
+    fun getAllbookmarkedNotes()= runBlocking {
+        val user = User(email = "aks@gmail.com", password = "12345")
+        repository = UserRepository()
+        val expectedResult = true
+        val response = repository.checkUser(user)
+        ServiceBuilder.token = "Bearer " + response.token
+        ServiceBuilder.id = response.id
+        val repo=WorkOutRepository()
+        val actualResult=repo.getallProduct(ServiceBuilder.id!!).success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
 }
